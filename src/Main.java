0c2c577fd3b8cf5b004d94c1c880d6b6ca96be2e
Nikita Kozlov <kozlov22.00@mail.ru>
2022-11-19 17:08:22 +0400

    Заготовка для торрента.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.io.IOException;
+
 import BitTorrent.Bencode;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        char x = 0;
+        char x = '1';
         System.out.println("Start main: " + x);
-        Bencode code = new Bencode(123);
-        code.printSymbol(x);
+        Bencode.printSymbol(x);
     }
 }
