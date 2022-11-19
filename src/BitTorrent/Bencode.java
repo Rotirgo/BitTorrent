65675b05e32fe6736d37a96a526422a2159ef10f
XBACTYN <dreamquarz@gmail.com>
2022-11-19 20:26:06 +0400

    Парсим файл

--- src/BitTorrent/Bencode.java
+++ src/BitTorrent/Bencode.java
@@ -1,8 +1,41 @@
 package BitTorrent;
 
+import unet.bencode.variables.BencodeObject;
+
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public interface Bencode {
 
     static void printSymbol(char c){
         System.out.println("Symbol: " + c);
+    }
+
+    static ArrayList<String> GetData(String file) throws IOException {
+        byte[] b = Files.readAllBytes(Paths.get(file));
+        return Separate(new BencodeObject(b).toString());
+    }
+
+    static ArrayList<String> Separate(String data){
+        ArrayList<Pattern> patterns = new ArrayList<Pattern>();
+        System.out.println(data);
+        patterns.add(Pattern.compile("comment.+(http.+\u001b\\[0m)"));
+        //patterns.add(Pattern.compile("comment:(.{15,80}).{1,3}created"));
+        //patterns.add(Pattern.compile("length:([0-9]{4,32})"));
+        //patterns.add(Pattern.compile("name:(.{10,}\\.{2,10})"));
+        ArrayList<String> lst = new ArrayList<>();
+        for(Pattern pattern : patterns)
+        {
+            Matcher matcher  =  pattern.matcher(data);
+            while(matcher.find())
+                lst.add(matcher.group(1));
+        }
+
+        return lst;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
+import java.awt.*;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import BitTorrent.Bencode;
+import unet.bencode.variables.BencodeObject;
+
 
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        char x = '1';
-        System.out.println("Start main: " + x);
-        Bencode.printSymbol(x);
+        String file_path = "C:\\Users\\user\\Downloads\\Overlord + Raising Hell [P] [RUS(2) + ENG RUS + ENG] (2007) (1.4) [P2P] [rutracker-6264723].torrent";
+        ArrayList<String> data =Bencode.GetData(file_path);
+        System.out.println("list "+data);
+
     }
-}
+
+
+};
+
