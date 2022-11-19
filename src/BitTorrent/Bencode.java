d628b5d5ec112b49add31bd135d5f9a5bb210d7b
Nikita Kozlov <kozlov22.00@mail.ru>
2022-11-19 22:05:13 +0400

    Запарсили данные, кроме info. Сделать хэш всего info.

--- src/BitTorrent/Bencode.java
+++ src/BitTorrent/Bencode.java
@@ -1,6 +1,9 @@
 package BitTorrent;
 
+import unet.bencode.Bencoder;
+import unet.bencode.variables.BencodeBytes;
 import unet.bencode.variables.BencodeObject;
+import unet.bencode.variables.BencodeVariable;
 
 import java.awt.*;
 import java.io.IOException;
@@ -8,6 +11,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public interface Bencode {
@@ -18,16 +26,20 @@ public interface Bencode {
 
     static ArrayList<String> GetData(String file) throws IOException {
         byte[] b = Files.readAllBytes(Paths.get(file));
+//        Bencoder benc = new Bencoder();
+//        Map<BencodeBytes, BencodeVariable> dict = benc.decodeObject(b, 0);
+//        System.out.println(dict.values());
         return Separate(new BencodeObject(b).toString());
     }
 
     static ArrayList<String> Separate(String data){
         ArrayList<Pattern> patterns = new ArrayList<Pattern>();
         System.out.println(data);
-        patterns.add(Pattern.compile("comment.+(http.+\u001b\\[0m)"));
-        //patterns.add(Pattern.compile("comment:(.{15,80}).{1,3}created"));
-        //patterns.add(Pattern.compile("length:([0-9]{4,32})"));
-        //patterns.add(Pattern.compile("name:(.{10,}\\.{2,10})"));
+        patterns.add(Pattern.compile("announce.+(http.+\u001b\\[0m)"));
+        patterns.add(Pattern.compile("length.{12}([0-9]{8,20})\u001b\\[0m"));
+        patterns.add(Pattern.compile("name.{12}(\\w.+)\u001b\\[0m"));
+        patterns.add(Pattern.compile("piece length.{12}([0-9]{1,20})\u001b\\[0m"));
+        patterns.add(Pattern.compile("piece.{12,20} \\{ (.+) \\}\u001b\\[0m"));
         ArrayList<String> lst = new ArrayList<>();
         for(Pattern pattern : patterns)
         {
@@ -37,5 +49,12 @@ public interface Bencode {
         }
 
         return lst;
+    }
+
+    static Map<String, String> getData(String bencode){
+        Pattern format = Pattern.compile("d\\d+");
+        System.out.println(format);
+        return null;
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.awt.*;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Base64;
 
 import BitTorrent.Bencode;
 import unet.bencode.variables.BencodeObject;
@@ -12,9 +14,13 @@ import unet.bencode.variables.BencodeObject;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String file_path = "C:\\Users\\user\\Downloads\\Overlord + Raising Hell [P] [RUS(2) + ENG RUS + ENG] (2007) (1.4) [P2P] [rutracker-6264723].torrent";
+        String file_path = "C:\\Users\\Никита\\Desktop\\Overlord__Raising_Hell_P__RUS_2__ENG_RUS__ENG__2007" +
+                "__1_4__P2P__rutracker-6264723_torrent.torrent";
         ArrayList<String> data =Bencode.GetData(file_path);
         System.out.println("list "+data);
+        byte[] b = Base64.getDecoder().decode(data.get(4));
+        FileOutputStream fos = new FileOutputStream("pathname");
+        fos.write(b);
 
     }
 
