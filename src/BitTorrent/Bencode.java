a3c3f45f54f7d0c5816f393cc47215817021a03a
Nikita Kozlov <kozlov22.00@mail.ru>
2022-11-21 21:41:04 +0400

    сделано подключение к трекеру.найти работающий торрент файл, к которому можно подключиться и получить пиры.

--- src/BitTorrent/Bencode.java
+++ src/BitTorrent/Bencode.java
@@ -2,6 +2,7 @@ package BitTorrent;
 
 import unet.bencode.variables.BencodeObject;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -32,6 +33,16 @@ public interface Bencode {
                 lst.add(matcher.group(1));
         }
         return lst;
+    }
+
+    static byte[] getInfo(ArrayList<String> data, byte[] pieces){
+        String piecesStr = new String(pieces, StandardCharsets.UTF_8);
+        int length = Integer.parseInt(data.get(1));
+        String name = data.get(3);
+        int pieceLength = Integer.parseInt(data.get(2));
+        String result = "d6:lengthi"+length+"e4:name"+name.length()+":"+name+
+                "12:piece lengthi"+pieceLength+"e6pieces:"+pieces.length+":"+piecesStr+"e";
+        return result.getBytes(StandardCharsets.UTF_8);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Base64;
@@ -13,29 +12,19 @@ public class Main {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         String file_path = "C:\\Users\\Никита\\Desktop\\MoralPsychHandbook.torrent";
                 //"C:\\Users\\Никита\\Desktop\\ComputerNetworks.torrent";
-                //"C:\\Users\\Никита\\Desktop\\Overlord__Raising_Hell_P__RUS_2__ENG_RUS__ENG__2007" +
-                //"__1_4__P2P__rutracker-6264723_torrent.torrent";
+                //"C:\\Users\\Никита\\Desktop\\Overlord__Raising_Hell_P__RUS_2__ENG_RUS__ENG__2007__1_4__P2P__rutracker-6264723_torrent.torrent";
+        // Парсинг нужных данных
         ArrayList<String> data =Bencode.GetData(file_path);
         System.out.println("list "+data);
 
-        // piece приводим к первоначальному виду
-        byte[] pieces = Base64.getDecoder().decode(data.get(4));  // разбить по 20 байт
-        String piecesStr = new String(pieces, StandardCharsets.UTF_8);
-
-        // из data надо еще сделать по правилам bencode данные info
+        // Создание info, чтобы потом хэшировать
+        byte[] pieces = Base64.getDecoder().decode(data.get(4));
         String announce = data.get(0);
-        int length = Integer.parseInt(data.get(1));
-        String name = data.get(3);
-        int pieceLength = Integer.parseInt(data.get(2));
 
-        String infoBencode = "d6:lengthi"+length+"e4:name"+name.length()+":"+name+
-                "12:piece lengthi"+pieceLength+"e6pieces:"+pieces.length+":"+piecesStr+"e";
-        byte[] info = infoBencode.getBytes(StandardCharsets.UTF_8);
+        byte[] info = Bencode.getInfo(data, pieces);
         String infoHashHex = SHA1.toSHA1(info);
-        String infoHash = Options.getInfoHashForRequest(infoHashHex, "%"); // 06661137d3fabf9b27e75deb4d0a0906bbe789ad
+        String infoHash = Options.getInfoHashForRequest(infoHashHex, "%");
 
-//        System.out.println("\n");
-//
 //        FileOutputStream fos = new FileOutputStream("pathname");
 //        fos.write(info);
 
@@ -56,7 +45,6 @@ public class Main {
         UrlConnectionTracker.Connect(request.toString());
 
     }
-
 
 };
 
